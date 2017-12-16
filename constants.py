8a29018eee24560ab86adb7fa3f766e28f2b88f8
Tim MacNeil <github@rabidbulldog.tk>
2017-12-16 14:12:27 -0400

    Imported the work so far into the base game dev.

---
+++ constants.py
@@ -0,0 +1,7 @@
+# Define configuration variables here
+RESOURCES_DIR = 'data'
+
+HERO_MOVE_SPEED = 200  # pixels per second
+HERO_SPRITESHEET = 'hero2.png'
+
+MAP_FILENAME = 'grasslands.tmx'
---
+++ functions.py
@@ -0,0 +1,18 @@
+import os.path
+import pygame
+from constants import *
+
+# simple wrapper to keep the screen resizeable
+def init_screen(width, height):
+	screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
+	return screen
+
+
+# make loading maps a little easier
+def get_map(filename):
+	return os.path.join(RESOURCES_DIR, filename)
+
+
+# make loading images a little easier
+def load_image(filename):
+	return pygame.image.load(os.path.join(RESOURCES_DIR, filename))
