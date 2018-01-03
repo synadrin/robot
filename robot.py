dbcc9c91f7936747af2db7a6bde5e03ed47731f2
Tim MacNeil <github@rabidbulldog.tk>
2018-01-03 10:47:54 -0400

    Updated properties in Tiled map to reflect Python naming conventions.

--- robot.py
+++ robot.py
@@ -62,8 +62,8 @@ class QuestGame(object):
                     object.width, object.height))
             elif object.type == 'sprite':
                 # Load sprite from JSON
-                target_x = int(object.targetX) * tmx_data.tilewidth
-                target_y = int(object.targetY) * tmx_data.tileheight
+                target_x = int(object.target_x) * tmx_data.tilewidth
+                target_y = int(object.target_y) * tmx_data.tileheight
                 npc = character.npc(object.name, [object.x, object.y], [target_x, target_y])
                 self.npcs.append(npc)
                 self.group.add(npc)
